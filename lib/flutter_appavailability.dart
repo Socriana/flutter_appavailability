326918bb2fdab0203375e2cb35d3fdc3e6d6598c
Cristiano Andreola <cristianoandreola@hotmail.com>
2021-05-03 11:41:22 -0300

    Convert to null safety

--- lib/flutter_appavailability.dart
+++ lib/flutter_appavailability.dart
@@ -5,8 +5,7 @@ import 'package:flutter/services.dart';
 
 /// Main class of the plugin.
 class AppAvailability {
-  static const MethodChannel _channel =
-      const MethodChannel('com.pichillilorenzo/flutter_appavailability');
+  static const MethodChannel _channel = const MethodChannel('com.pichillilorenzo/flutter_appavailability');
 
   /// Check if an app is available with the given [uri] scheme.
   ///
@@ -21,30 +20,19 @@ class AppAvailability {
   ///   "versionCode": "",
   ///   "version_name": ""
   /// }
-  static Future<Map<String, String>> checkAvailability(String uri) async {
+  static Future<Map<String, String>?>? checkAvailability(String uri) async {
     Map<String, dynamic> args = <String, dynamic>{};
     args.putIfAbsent('uri', () => uri);
 
     if (Platform.isAndroid) {
       Map<dynamic, dynamic> app = await _channel.invokeMethod("checkAvailability", args);
-      return {
-        "app_name": app["app_name"],
-        "package_name": app["package_name"],
-        "versionCode": app["versionCode"],
-        "version_name": app["version_name"]
-      };
-    }
-    else if (Platform.isIOS) {
+      return {"app_name": app["app_name"], "package_name": app["package_name"], "versionCode": app["versionCode"], "version_name": app["version_name"]};
+    } else if (Platform.isIOS) {
       bool appAvailable = await _channel.invokeMethod("checkAvailability", args);
       if (!appAvailable) {
         throw PlatformException(code: "", message: "App not found $uri");
       }
-      return {
-        "app_name": "",
-        "package_name": uri,
-        "versionCode": "",
-        "version_name": ""
-      };
+      return {"app_name": "", "package_name": uri, "versionCode": "", "version_name": ""};
     }
 
     return null;
@@ -55,23 +43,18 @@ class AppAvailability {
   /// Get the list of all installed apps, where
   /// each app has a form like [checkAvailability()].
   static Future<List<Map<String, String>>> getInstalledApps() async {
-    List<dynamic> apps = await _channel.invokeMethod("getInstalledApps");
+    List<dynamic>? apps = await _channel.invokeMethod("getInstalledApps");
     if (apps != null && apps is List) {
-      List<Map<String, String>> list = new List();
+      List<Map<String, String>> list = new List.empty();
       for (var app in apps) {
         if (app is Map) {
-          list.add({
-            "app_name": app["app_name"],
-            "package_name": app["package_name"],
-            "versionCode": app["versionCode"],
-            "version_name": app["version_name"]
-          });
+          list.add({"app_name": app["app_name"], "package_name": app["package_name"], "versionCode": app["versionCode"], "version_name": app["version_name"]});
         }
       }
 
       return list;
     }
-    return new List(0);
+    return new List.empty();
   }
 
   /// Only for **Android**.
@@ -93,14 +76,11 @@ class AppAvailability {
     args.putIfAbsent('uri', () => uri);
     if (Platform.isAndroid) {
       await _channel.invokeMethod("launchApp", args);
-    }
-    else if (Platform.isIOS) {
+    } else if (Platform.isIOS) {
       bool appAvailable = await _channel.invokeMethod("launchApp", args);
       if (!appAvailable) {
         throw PlatformException(code: "", message: "App not found $uri");
       }
     }
-
   }
-
 }
